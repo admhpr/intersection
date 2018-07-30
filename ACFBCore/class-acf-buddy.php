0d785497c4e7b524b9ddb127d7f2954153ced7ca
adam <adam@adamharpur.com>
2018-07-29 21:36:12 -0500

    fix(master): refactor logic

--- ACFBCore/class-acf-buddy.php
+++ ACFBCore/class-acf-buddy.php
@@ -36,7 +36,8 @@ namespace ACFBCore{
                         'post_status'      => 'any',
                         'update_post_meta_cache' => false
                     ));
-    
+                    
+                    $this->clean_sections = [];
                     foreach ( $sections as $section ) {
                         
                  
@@ -44,7 +45,7 @@ namespace ACFBCore{
                         $layout_type = $section->post_excerpt;
                       
     
-                        $this->process_layout($layout, $layout_type);
+                        $this->process_layout($layout, $layout_type, $requested_sections);
                         
                         
                     }
@@ -55,13 +56,10 @@ namespace ACFBCore{
                 return $this->clean_sections;
             }
 
-            // mock data for unit testing
-            // TODO:
-            // if(!empty($requested_sections)){
-            //     return $clean_requested_sections;
-            // }
+            if($phpunit){
+                $this->process_layout($layout, $layout_type);
+            }
 
-            $this->process_layout($layout, $layout_type);
             return $this->clean_sections;  
         }
     
@@ -72,47 +70,61 @@ namespace ACFBCore{
          */
         public function render_fields($partials, $exclude = []){
 
-            foreach($partials as $sections  ){
-                foreach($sections as $section => $contents){
-                    if($section !== 'page_components' && !in_array($section, $exclude)){
-                        $folder = '/partials/sections/' . str_replace('_', '-', $section ) . "/";
-                    foreach($contents as $fields){
-                            $path = $folder . str_replace('_', '-', $fields['acf_fc_layout'] ) . '.php';
-                            include(locate_template( $path ));	                   
-                        }
+            foreach($partials as $section ){
+                $section_type = $section['acf_fc_layout'];
+         
+                if($section_type !== 'page_components' && !in_array($section_type, $exclude)){
+                    $folder = '/partials/sections/' . str_replace('_', '-', $section['acf_fc_layout'] ) . "/";
+                    
+                    foreach($section[$section_type] as $contents){
+                        $path = $folder . str_replace('_', '-', $contents['acf_fc_layout'] ) . '.php';
+                        include(locate_template( $path ));	                   
                     }
                 }
+    
             }
 
         }
 
         public function process_layout($layout,$layout_type, $requested_sections=[]){
-            /***
-             * 
-             *  FIXME: requested_sections is a place holder at the moment. 
-             *  write unit test
-             *  refactor
-             */
+        
+
             if($layout_type !== 'page_components'){
                 
-                foreach($layout as $key => $value){
-
-                    if( count($requested_sections) == 0){
-                        // TODO:
-                    }else{
-                        // TODO:
-                    }
-                    $this->clean_requested_sections[$key][$value["acf_fc_layout"]] = [];
-                    $this->clean_sections[$key][$value["acf_fc_layout"]] = [];
-                    foreach($value["section_layout"] as $field_values){
+                foreach($layout as $index => $partial){
+          
+                    foreach($partial["section_layout"] as $field_values){
+                        
                         if(is_null($field_values)){
                             $field_values = "";
                         }
-                        if(in_array($value['acf_fc_layout'], $requested_sections)){
-                            array_push($this->clean_requested_sections[$key][$value["acf_fc_layout"]],$field_values);
+
+                        
+
+                        // TODO: further abstract
+                        $section = $partial['acf_fc_layout'];
+                        if( count($requested_sections) == 0){
+                            
+                            if( $index <= ( count($layout) - 1 )){
+                                array_push( $this->clean_sections, array());
+                            }
+                            
+                            $this->add_to_clean_sections($index, $section, $field_values);
+
                         }else{
-                            array_push($this->clean_sections[$key][$value["acf_fc_layout"]],$field_values);
+                          
+                            if( in_array( $section, $requested_sections ) ){
+                                
+
+                                if($index <= ( count($layout) - 1 )){
+                                    array_push( $this->clean_sections, array());
+                                }
+
+                                $this->add_to_clean_sections($index, $section, $field_values);
+                            
+                            }
                         }
+                   
                     }
                 }
 
@@ -121,6 +133,12 @@ namespace ACFBCore{
             return $this->clean_sections;
                 
              
+        }
+
+
+        public function add_to_clean_sections($index, $section, $field_values ){
+            $this->clean_sections[$index]["acf_fc_layout"] = $section;
+            $this->clean_sections[$index][$section][] = $field_values;
         }
 
         public function create_dummy_layout(){
