4a2f829a5ef8e720c726debb818405bf7bf82ae1
adam <adam@adamharpur.com>
2018-08-24 13:51:09 -0500

    feat(master): added aI1ec support

--- IntersectionHandler/class-ai1ec.php
+++ IntersectionHandler/class-ai1ec.php
@@ -4,42 +4,58 @@
     namespace IntersectionHandler {
         
         use WP_Query;
-        // use HelperClass?
         class Ai1EC implements interface_handler  {
+        
+        protected $EVENT_TABLE = "wp_ai1ec_events";
+        protected $POST_TABLE = "wp_posts";
+        protected $TERM_RELATIONSHIP_TABLE = "wp_term_relationships";
+        protected $TERM_TAXONOMY_TABLE = "wp_term_taxonomy";
+        protected $TERM_TABLE = "wp_terms";
 
-    
         public function __construct(){
    
         }
 
         public function prepare(array $requested_sections = [], $phpunit=false){
-            echo "here";
-            $this->get_latest_events();
+            $events = $this->get_events();
         }
 
         public function render(array $partials, $exclude = []){
 
         }
 
-        public function get_latest_events(){
-   
-            $args = array(
-                'posts_per_page'  => 4,
-                'paged' => get_query_var('paged'),
-                'post_type'=> 'ai1ec_event',
-                'orderby' => 'post__in'
-            );
+        public function get_events(){
+            
+            global $wpdb;
 
+            $sql = "SELECT * FROM {$this->EVENT_TABLE} e
+                    LEFT JOIN {$this->POST_TABLE} p ON e.post_id = p.ID 
+                    LEFT JOIN {$this->TERM_RELATIONSHIP_TABLE} rel ON rel.object_id = p.ID 
+                    LEFT JOIN {$this->TERM_TAXONOMY_TABLE} tax ON tax.term_taxonomy_id = rel.term_taxonomy_id 
+                    LEFT JOIN {$this->TERM_TABLE} t ON t.term_id = tax.term_id
+                    ORDER BY FROM_UNIXTIME(e.start,'%Y %D %M %h:%i:%s %x') DESC";
 
-            $events_added = new WP_Query( $args );
-
-
-            // the Loop
-            while ( $events_added->have_posts() ) : $events_added->the_post();
-                // $event = Ai1ec_Events_Helper::get_event($post->ID);
-                // $event = $events_added->the_post();
-                
-            endwhile;
+            $events = $wpdb->get_results($sql, ARRAY_A);
+            
+            $upcoming_events = [];
+            $featured_events = [];
+            
+            foreach($events as $event){
+                $now = time();
+                if($event['end'] >= $now){
+                    $correct_setting = (strtolower($event['taxonomy']) === 'events_categories' || strtolower($event['taxonomy']) === 'events_tags');
+                    $featured = strtolower($event['name']) == "featured"; 
+                    $is_featured = $correct_setting && $featured;
+                    if($is_featured){
+                        $featured_events[] = $event;
+                    }else{
+                        $upcoming_events[] = $event;
+                    }
+                }
+            }
+            $sorted_events = array_merge($featured_events, $upcoming_events);
+        
+            return $sorted_events;
         }
 
         public function get_featured_events(){
